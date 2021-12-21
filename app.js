a5c3a8f406cef230bd03a4aea91969d0c6c2c4df
Kritartha Bikram Shah <67311836+thisiskritartha@users.noreply.github.com>
2021-12-21 22:34:09 +0545

    Mangoose connect url updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDb");
+mongoose.connect("mongodb+srv://admin-krit:Test123@cluster0.vuae7.mongodb.net/todolistDb");
 
 const itemsSchema = {
   name: String,
@@ -123,6 +123,11 @@ app.post("/delete", function(req, res){
 
 })
 
-app.listen("3000", function() {
-  console.log("The Server is running on Port 3000");
+let port = process.env.PORT;
+if(port == NULL || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("The Server is running Successfully...");
 });
